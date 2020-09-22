76aaf0414900dc3236d23690d9015ac2dc6add3e
Keith Pohl <keith.pohl82@gmail.com>
2020-09-22 18:13:09 -0500

    Initial Commit

---
+++ SilentRed/Intermediate/Build/Win64/SilentRedEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "U:/SilentRed/SilentRed/Intermediate/Build/Win64/SilentRedEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ SilentRed/Source/SilentRed/SilentRedGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SilentRedGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SILENTRED_API ASilentRedGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
